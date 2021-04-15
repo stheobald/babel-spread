785c37749590965748fc04f4c3b0b554ac9485b6
Simeon <simeon@theobald.nz>
2021-04-15 18:01:47 +1200

    further simplification

--- babel.config.js
+++ babel.config.js
@@ -1,23 +1,8 @@
 module.exports = function(api) {
-  // api.cache(false);
-  api.cache(true);
+  api.cache(false)
   return {  
     presets: [
       '@babel/env'
-    ],
-    plugins: [
-      [
-        '@babel/module-resolver',
-        {
-          root: ['src','node_modules'],
-          alias: {
-            helpers: "./src/helpers",
-            // '~': "./src",
-            "^~(.+)": "./src\\1"
-          },
-          loglevel: 'verbose'
-        },
-      ]
-    ],
+    ]    
   };
 };
